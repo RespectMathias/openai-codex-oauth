d0b4dfffcc00812cba1ffb05e264190b929e89f2
Mathias <tisen2013@gmail.com>
2026-05-19 02:30:34 +0200

    fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/node/index.ts', 'src/proxy.ts'],
-  format: ['esm'],
+  format: ['cjs'],
   dts: true,
   sourcemap: true,
   clean: true,
